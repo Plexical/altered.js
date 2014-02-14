4889a209daac88391821bde4d068bb075cbe7c8d
Jacob Oscarson <jacob@plexical.com>
2014-02-14 11:15:59 +0100

    Single function base functionallity

--- altered.js
+++ altered.js
@@ -1,5 +1,26 @@
 ;(function(module, global) {
   var state = function(orig, changes, run) {
+    var saved = {};
+
+    for(var key in changes) {
+      saved[key] = orig[key];
+      if(changes[key] == undefined)
+        delete orig[key];
+      else
+        orig[key] = changes[key];
+    }
+
+    try {
+      run()
+    } finally {
+      for(var key in changes) {
+        if(saved[key] == undefined)
+          delete orig[key];
+        else
+          orig[key] = saved[key];
+      }
+    }
+
     return orig;
   }
 
--- spec/altered-spec.js
+++ spec/altered-spec.js
@@ -1,1 +1,39 @@
-var state = require('./../altered').state;
+var state = require('./../altered').state,
+    should = require('should');
+
+describe("The state() function", function() {
+  it("temporary adds new properties", function() {
+    var ob = {a: 1};
+    state(ob, {b: 2}, function() {
+      ob.a.should.be.exactly(1);
+      ob.b.should.be.exactly(2);
+    });
+    Object.keys(ob).should.eql(['a']);
+  });
+  it("temporary overwrites properties", function() {
+    var ob = {a: 1};
+    state(ob, {a: 2}, function() {
+      ob.a.should.be.exactly(2);
+    });
+    ob.a.should.be.exactly(1);
+  });
+  it("can 'forget' undefined properties", function() {
+    var ob = {a: 1, b: 2};
+    state(ob, {b: undefined}, function() {
+      Object.keys(ob).should.eql(['a']);
+    });
+    ob.should.eql({a: 1, b: 2});
+  });
+  it("restores correctly even if callback throws", function() {
+    var ob = {a: 1};
+    try {
+      state(ob, {a: 2}, function() {
+        ob.should.eql({a: 2});
+        throw 'Ops!'
+      });
+    } catch(e) {
+      // <- NB exception still thrown and propagates
+      ob.should.eql({a: 1});
+    }
+  });
+});
