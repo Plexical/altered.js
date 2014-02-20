ad69e3b61765a24736c0ebde70bf9ae8da63a6f9
Jacob Oscarson <jacob@plexical.com>
2014-02-20 14:01:04 +0100

    Common change function

--- altered.js
+++ altered.js
@@ -1,24 +1,25 @@
 ;(function(module, global) {
+  var modify = function(orig, key, change) {
+    var saved = orig[key];
+    if(change == undefined)
+      delete orig[key];
+    else
+      orig[key] = change;
+
+    return saved;
+  }
+
   var state = function(orig, changes, run) {
     var saved = {};
 
-    for(var key in changes) {
-      saved[key] = orig[key];
-      if(changes[key] == undefined)
-        delete orig[key];
-      else
-        orig[key] = changes[key];
-    }
+    for(var key in changes)
+      saved[key] = modify(orig, key, changes[key]);
 
     try {
       run()
     } finally {
-      for(var key in changes) {
-        if(saved[key] == undefined)
-          delete orig[key];
-        else
-          orig[key] = saved[key];
-      }
+      for(var key in changes)
+        modify(orig, key, saved[key]);
     }
 
     return orig;
