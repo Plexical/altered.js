bb72b5ced09ef8605733bbda675f1ba5406860fd
Jacob Oscarson <jacob@plexical.com>
2014-02-21 16:13:21 +0100

    Implemented two-step change/restore

--- altered.js
+++ altered.js
@@ -9,23 +9,29 @@
     return saved;
   }
 
-  var state = function(orig, changes, run) {
+  var change = function(orig, changes) {
     var saved = {};
-
     for(var key in changes)
       saved[key] = modify(orig, key, changes[key]);
+
+    return function() {
+      for(var key in saved) modify(orig, key, saved[key]);
+    };
+  }
+
+  var state = function(orig, changes, run) {
+    var restore = change(orig, changes);
 
     try {
       run()
     } finally {
-      for(var key in changes)
-        modify(orig, key, saved[key]);
+      restore();
     }
 
     return orig;
   }
 
   module.exports = {
-    state: state
+    state: state, change: change
   }
 })(module, global);
--- spec/altered-spec.js
+++ spec/altered-spec.js
@@ -1,4 +1,7 @@
-var state = require('./../altered').state,
+var altered = require('./../altered');
+
+var state = altered.state,
+    change = altered.change,
     should = require('should');
 
 describe("The state() function", function() {
@@ -35,5 +38,33 @@ describe("The state() function", function() {
       // <- NB exception still thrown and propagates
       ob.should.eql({a: 1});
     }
+  });
+});
+
+describe("two step alteration", function() {
+  var ob = null;
+
+  beforeEach(function() {
+    ob = {a: 1};
+  });
+
+  it("temporary adds new properties", function() {
+    var restore = change(ob, {b: 2});
+    ob.should.eql({a: 1, b: 2});
+    restore();
+    ob.should.eql({a: 1});
+  });
+  it("temporary overwrites properties", function() {
+    var restore = change(ob, {a: 2});
+    ob.should.eql({a: 2});
+    restore();
+    ob.should.eql({a: 1});
+  });
+  it("can 'forget' undefined properties", function() {
+    var o2 = {a: 1, b: 2};
+    var restore = change(o2, {b: undefined});
+    o2.should.eql({a: 1});
+    restore();
+    o2.should.eql({a: 1, b: 2});
   });
 });
